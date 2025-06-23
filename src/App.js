c2f321946fcd7581873638026ca06e280a0bf978
carlapdl <macarla.poncedeleon@gmail.com>
2025-06-23 14:13:48 +0800

    Pre-Vite Migration Backup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import Header from './components/Header';
+import ProductGrid from './components/ProductGrid';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        <h1 className="category-title">Women</h1>
+        <ProductGrid />
+      </main>
     </div>
+
   );
 }
 
